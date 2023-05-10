9fd126c6f493afeee0789ca94b8ffb34c017d35f
tamerlane4 <fennell.lance@gmail.com>
2023-05-10 01:25:41 -0500

    backend

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require('cors')
+
+
+
+app.use(cors())
 
 /*Middleware---------------------------------------------------
 const requestLogger = (request, response, next) => {
@@ -52,7 +57,7 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
-
+app.use(express.static('build'))
 const generateId = () => {
   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
   return maxId + 1;
@@ -112,7 +117,8 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3000;
+//const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
